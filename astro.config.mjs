6c8033b647612bf5d6f45dd4897f7b33c513d905
cjdomacena <cjdomacena@yahoo.com>
2022-10-05 23:48:55 -0400

    build: :construction_worker: Added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
